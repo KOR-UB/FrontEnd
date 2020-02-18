2b64a4727a9b08860d2d3f86657c2046eaca9fbc
UB <yyb2535@gmail.com>
2020-02-18 19:06:49 +0900

    Push!!
    push!

--- js/ToDoListManager.js
+++ js/ToDoListManager.js
@@ -25,9 +25,10 @@ greetings = document.querySelector(".js-greetings");
 const USER_LS = "currentUser",
 SHOWING_CN = "showing";
 
+
 function loadName()
 {
-    const currentUser = localStorage.getItem(USER_LS)
+    const currentUser = localStorage.getItem(USER_LS);
     if(currentUser === null)
     {
         askForName();
@@ -37,8 +38,12 @@ function loadName()
         paintGreeting(currentUser);
     }
 }
+function saveName(text)
+{
+    localStorage.setItem(USER_LS, text)
+}
 function askForName()
-{   
+{
     form.classList.add(SHOWING_CN);
     form.addEventListener("submit", handleSubmit);
 }
@@ -49,15 +54,82 @@ function handleSubmit(event)
     paintGreeting(currentUser);
     saveName(currentUser);
 }
-function saveName(text)
-{
-    localStorage.setItem(USER_LS, text);
-}
 function paintGreeting(text)
 {
     form.classList.remove(SHOWING_CN);
     greetings.classList.add(SHOWING_CN);
     greetings.innerText = `안녕하세요 ${text}`
 }
+
 loadName();
+
+//#endregion
+//#region toDoList
+const toDoForm = document.querySelector(".js-toDoForm"),
+toDoInput = toDoForm.querySelector("input"),
+toDoList = document.querySelector(".js-toDoList");
+
+const TODOS_LS = "toDos";
+let toDos = [];
+
+function saveToDos()
+{
+    localStorage.setItem(TODOS_LS, JSON.stringify(toDos));
+}
+function deleteToDo(event)
+{
+    const btn = event.target;
+    const li = btn.parentNode;
+    toDoList.removeChild(li);
+    const cleanToDos = toDos.filter(function(toDo)
+    {
+       return toDo.id !== parseInt(li.id);
+    });
+    toDos = cleanToDos;
+    saveToDos();
+}
+function paintToDo(text)
+{
+    const li = document.createElement("li");
+    const span = document.createElement("span");
+    const delBtn = document.createElement("button");
+    const newId = toDos.length +1;
+    delBtn.innerText = "X";
+    span.innerText = text;
+    delBtn.addEventListener("click",deleteToDo);
+    li.appendChild(span);
+    li.appendChild(delBtn);
+    li.id = newId
+    toDoList.appendChild(li);
+    const toDoObj ={
+        text : text,
+        id : newId
+    };
+    toDos.push(toDoObj);
+    saveToDos();
+}
+function submitHandle(event)
+{
+    event.preventDefault();
+    const currentvalue = toDoInput.value;
+    paintToDo(currentvalue);
+    toDoInput.value = "";
+}
+function toDoInit()
+{
+    loadToDos();
+    toDoForm.addEventListener("submit", submitHandle)
+}
+function loadToDos()
+{
+    const loadedToDos = localStorage.getItem(TODOS_LS);
+    if(loadedToDos !== null)
+    {
+        const parsedToDos = JSON.parse(loadedToDos);
+        parsedToDos.forEach(function(toDo){
+            paintToDo(toDo.text)
+        });
+    }
+}
+toDoInit();
 //#endregion
